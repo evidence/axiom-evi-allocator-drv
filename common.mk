525704b476fa3af685f5a07aab3a36e9f84b5a97
Massimiliano Giorgi <m.giorgi@evidence.eu.com>
2017-08-03 09:49:47 +0000

    add support for X86 native compilation (on ubuntu xenial)

--- common.mk
+++ common.mk
@@ -1,7 +1,11 @@
 
 COMMKFILE_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
-CCARCH := aarch64
+ifeq ($(FS),x86)
+  undefine CCARCH
+else
+  CCARCH := aarch64
+endif
 
 BUILDROOT := ${COMMKFILE_DIR}/../axiom-evi-buildroot
 OUTPUT_DIR := ${BUILDROOT}/../output
@@ -13,9 +17,18 @@ ifeq ($(KERN),br)
     KERNELDIR := ${OUTPUT_DIR}/build/linux-custom
     CC := $(OUTPUT_DIR)/host/usr/bin/aarch64-buildroot-linux-gnu-gcc
 else
+ifeq ($(KERN),x86)
+    ifeq ($(FS),x86)
+	    KERNELVER :=  4.4.0-21-generic
+    else
+	    KERNELVER :=  $(shell uname -r)
+    endif
+    KERNELDIR := /lib/modules/$(KERNELVER)/build
+else
     CROSS_COMPILE := ARCH=arm64 CROSS_COMPILE=$(PETALINUX)/tools/linux-i386/aarch64-linux-gnu/bin/aarch64-linux-gnu-
     KERNELDIR := $(AXIOMBSP)/build/linux/kernel/link-to-kernel-build
     CC := $(PETALINUX)/tools/linux-i386/aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc
+endif
 endif
 
 #else
